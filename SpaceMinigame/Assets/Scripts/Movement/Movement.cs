b890f6f866595b20da88046556df1e0ffcc8be1a
27chandler <chandler27s@yahoo.com>
2020-07-16 00:44:34 +0100

    Added movement base class

---
+++ SpaceMinigame/Assets/Scripts/Movement/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    public float SetSpeed() => _speed;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void SimpleMove(Vector3 direction)
+    {
+        Vector3 directionNormalized = direction.normalized;
+        transform.position += directionNormalized * _speed * Time.deltaTime;
+    }
+
+    public void PhysicsMove(Vector3 direction)
+    {
+        Vector3 directionNormalized = direction.normalized;
+        rb.AddForce(direction * _speed * Time.deltaTime);
+    }
+}
