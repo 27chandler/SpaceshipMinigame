fd9f26ff1ae4f5d6eb89afe2a6ba505921c1d2ef
27chandler <chandler27s@yahoo.com>
2020-07-16 00:45:03 +0100

    Added input based movement

---
+++ SpaceMinigame/Assets/Scripts/Movement/InputMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMovement : Movement
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            SimpleMove(transform.up);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            SimpleMove(-transform.up);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            SimpleMove(transform.right);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            SimpleMove(-transform.right);
+        }
+    }
+}
