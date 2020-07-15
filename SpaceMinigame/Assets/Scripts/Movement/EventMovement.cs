53b56658bb5fd64ada7a47072930dced3cba45d4
27chandler <chandler27s@yahoo.com>
2020-07-16 00:44:48 +0100

    Added event based movement

---
+++ SpaceMinigame/Assets/Scripts/Movement/EventMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventMovement : Movement
+{
+    [SerializeField] private bool isRightActive = false;
+    [SerializeField] private bool isLeftActive = false;
+
+    private void Update()
+    {
+        if (isRightActive)
+        {
+            MoveRight();
+            isRightActive = false;
+        }
+
+        if (isLeftActive)
+        {
+            MoveLeft();
+            isLeftActive = false;
+        }
+    }
+
+    private void MoveRight()
+    {
+        PhysicsMove(transform.right);
+    }
+
+    private void MoveLeft()
+    {
+        PhysicsMove(-transform.right);
+    }
+}
