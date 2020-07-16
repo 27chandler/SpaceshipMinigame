253ef069cfdf5a96f5a08f34ffcaf44dbf825bcc
27chandler <chandler27s@yahoo.com>
2020-07-17 00:59:31 +0100

    Created the ship event system

---
+++ SpaceMinigame/Assets/Scripts/Events/ShipEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ShipEvents : MonoBehaviour
+{
+    [SerializeField] private float _shipThrust;
+
+    public static ShipEvents current;
+
+    public event Action<Vector3> onActivateEngine;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public void ActivateEngine(Vector3 thrust_direction)
+    {
+        if (onActivateEngine != null)
+        {
+            onActivateEngine(thrust_direction * _shipThrust);
+        }
+    }
+}
