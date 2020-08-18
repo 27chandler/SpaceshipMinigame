bdb2b3344536841ca785011139ae0b972e2bdb03
27chandler <chandler27s@yahoo.com>
2020-08-18 01:10:27 +0100

    Added fireball and wall of death

---
+++ SpaceMinigame/Assets/Scripts/Movement/AutoRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, _speed);
+    }
+}
