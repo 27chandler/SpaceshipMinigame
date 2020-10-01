1978cb8250e8d095fca665b4778d282a93b4c5d7
27chandler <chandler27s@yahoo.com>
2020-10-01 01:23:26 +0100

    Added sustain times for button presses when using charges

--- SpaceMinigame/Assets/Scripts/Events/ButtonActivate.cs
+++ SpaceMinigame/Assets/Scripts/Events/ButtonActivate.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ButtonActivate : MonoBehaviour
 {
-    [SerializeField] private Vector3 _thrustDirection;
+    [SerializeField] protected Vector3 _thrustDirection;
     [SerializeField] protected bool _canThrust = true;
     // Start is called before the first frame update
     void Start()
--- SpaceMinigame/Assets/Scripts/Events/ButtonChargeActivate.cs
+++ SpaceMinigame/Assets/Scripts/Events/ButtonChargeActivate.cs
@@ -6,15 +6,42 @@ public class ButtonChargeActivate : ButtonActivate
 {
     [SerializeField] private int _maxCharges;
     [SerializeField] private int _charges;
+    [Space]
+    [SerializeField] private float _sustainTime;
+
+    private float _sustainTimer = 0.0f;
+    private bool _isSustained = false;
 
 
     protected override void ActivateSecondaryEffects()
     {
+        if (_isSustained)
+            return;
+
         if (_charges > 0)
+        {
             _charges--;
+            _isSustained = true;
+            _sustainTimer = _sustainTime;
+        }
         else
+        {
             _canThrust = false;
+        }
 
         base.ActivateSecondaryEffects();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isSustained)
+            return;
+
+        _sustainTimer -= Time.deltaTime;
+
+        ShipEvents.current.ActivateEngine(_thrustDirection);
+
+        if (_sustainTimer < 0.0f)
+            _isSustained = false;
     }
 }
